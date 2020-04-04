478070390af0e0138f0bd7547a1f51f36c41a108
Jafaros <57791098+Jafaros@users.noreply.github.com>
2020-04-05 00:12:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ bot.on('message', message => {
 		seek: 0,
 		volume: 1
 	}
-	if(message.content.toLowerCase().startsWith("-play" || "-šuldohraj" || "-zmrde")){
+	if(message.content.toLowerCase().startsWith("-play", "-šuldohraj", "-zmrdehraj")){
 		let args = message.content.split(" ");
 		let url = args[1];
 			if (message.member.voice.channel) {
